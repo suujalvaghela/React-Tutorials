ddc035aa25c04c529adea81112b9407086ae48d8
sujal vaghela <1suujalvaghela1@gmail.com>
2024-12-26 21:40:31 +0530

    redux

---
+++ 11reduxtoolkit/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import TodosList from './components/TodosList'
+
+function App() {
+
+  return (
+    <div>
+      <AddTodo/>
+
+      <TodosList/>
+    </div>
+  )
+}
+
+export default App
---
+++ 11reduxtoolkit/src/sliceFolder/Slice.jsx
@@ -0,0 +1,36 @@
+import {createSlice , nanoid} from '@reduxjs/toolkit'
+
+const initialState = {
+    todos: [{ id: 7, text: 'Mehta Nidhi' }]
+}
+
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        updateTodo: (state, action) => {
+            const {id , text} = action.payload
+            const todo = state.todos.map((todo) => (todo.id === id))
+            if(todo){
+                todo.text = ''
+            }
+
+        },
+        
+    }
+})
+
+export const { addTodo , updateTodo , removeTodo } = todoSlice.actions
+
+export const todoReducer = todoSlice.reducer
