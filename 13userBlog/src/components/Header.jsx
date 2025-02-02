9c4bf9ef7fdc31a630babea6044fe9bdc277d4d6
sujal vaghela <1suujalvaghela1@gmail.com>
2025-02-02 20:00:00 +0530

    input , button making components!!

--- 13userBlog/src/components/Header.jsx
+++ 13userBlog/src/components/Header.jsx
@@ -1,8 +1,74 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { LogoutBtn, Logo, Container } from './InputBox.js'
+import { Link } from 'react-router-dom'
 
 function Header() {
+
+  const authStatus = useSelector((state) => (state.auth.status))
+
+  const navItems = [
+    {
+      name: 'home',
+      slug: '/',
+      active: true
+    },
+    {
+      name: 'login',
+      slug: '/login',
+      active: !authStatus
+    },
+    {
+      name: 'Signup',
+      slug: '/signup',
+      active: !authStatus
+    },
+    {
+      name: 'All posts',
+      slug: '/all-posts',
+      active: authStatus
+    },
+    {
+      name: 'Add post',
+      slug: '/add-post',
+      active: authStatus
+    }
+  ]
+
+  const navigate = useNavigate()
+
   return (
-    <div>Header</div>
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width='70px' />
+            </Link>
+          </div>
+          <ul className='flex ml-auto'>
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button onClick={() => navigate(item.slug)}
+                    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {
+              authStatus && (
+                <li>
+                  <LogoutBtn />
+                </li>
+              )
+            }
+          </ul>
+        </nav>
+      </Container>
+    </header>
   )
 }
 
--- 13userBlog/src/components/InputBox.js
+++ 13userBlog/src/components/InputBox.js
@@ -1,4 +1,7 @@
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Logo from "./LogoutBtn.jsx";
+import LogoutBtn from './LogoutBtn.jsx'
+import Container from './Container.jsx'
 
-export {Header , Footer}
+export {Header , Footer , Logo , LogoutBtn , Container}
